3dd053e42761acfa6033f1941e7424fc4e686f7c
morganmepham <morgancmepham@gmail.com>
2022-09-10 09:46:08 +0100

    Clicking language buttons now open respective 'vaults' No information yet

--- source/main.js
+++ source/main.js
@@ -34,3 +34,27 @@ passwordSubmitButton.addEventListener('click', checkPassword)
 
 
 //Display Info
+const buttons = document.querySelectorAll('.small-vaults-eventTarget')
+buttons.forEach(element => {
+    element.addEventListener('click', displayInfo)
+});
+
+
+function displayInfo(e){
+    const htmlInfo = document.querySelector('.unlocked-html')
+    const cssInfo = document.querySelector('.unlocked-css')
+    const jsInfo = document.querySelector('.unlocked-js')
+    const targetClass = e.target.classList
+
+    if(targetClass[0] === 'HTML-vault' || targetClass[0] === 'HTML-title' || targetClass[0] === 'html-emoji'){
+        htmlInfo.style.display = 'block'
+        unlockedDiv.style.display = 'none'
+    }else if(targetClass[0] === 'CSS-vault' || targetClass[0] === 'CSS-title' || targetClass[0] === 'css-emoji'){
+        cssInfo.style.display = 'block'
+        unlockedDiv.style.display = 'none'
+    
+    }else if(targetClass[0] === 'JS-vault' || targetClass[0] === 'JS-title' || targetClass[0] === 'js-emoji'){
+        jsInfo.style.display = 'block'
+        unlockedDiv.style.display = 'none'
+    }
+}
