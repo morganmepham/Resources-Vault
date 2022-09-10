41ecb918f353cd751ab6a0b29aaf548a5a61f744
morganmepham <morgancmepham@gmail.com>
2022-09-10 09:04:01 +0100

    Lock emoji now vanishes once vault is unlocked

--- source/main.js
+++ source/main.js
@@ -5,20 +5,32 @@ const passwordSubmitButton = document.querySelector('.password-submit')
 const passwordInput = document.querySelector('.password-input')
 let passwordOfTheDay = '1289'
 
+//Unlock Vault
 function lockClick(){
     passwordRequestDiv.style.display = 'flex'
 }
 
 function checkPassword(e){
-    e.preventDefault()
-    if(passwordInput.value == passwordOfTheDay){
-        unlockedDiv.style.display = 'grid'
-        passwordRequestDiv.style.display = 'none'
-    }else{
-        console.log('err')
+    try{
+        e.preventDefault()
+    }catch(error){
+        console.log(error)
+    }
+
+    try{
+        if(passwordInput.value == passwordOfTheDay){
+            unlockedDiv.style.display = 'grid'
+            passwordRequestDiv.style.display = 'none'
+            lock.style.display = 'none'
+        }
+    }catch(error){
+        console.log(error)
     }
 }
 
 //event handlers
 lock.addEventListener('click', lockClick)
 passwordSubmitButton.addEventListener('click', checkPassword)
+
+
+//Display Info
